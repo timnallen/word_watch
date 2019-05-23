c70f898184941923d29269fa7b236b78a9a23e48
Tim Allen <tnelsallen3@gmail.com>
2019-05-23 10:55:26 -0600

    Add style to the top word

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ $(document).ready(() => {
   var getValue = function() {fetch("https://wordwatch-api.herokuapp.com/api/v1/top_word")
     .then(response => response.json())
     .then(results => {
-      let word = Object.keys(results.word)
-      $('h3').text(`Top results from Word Watch API: ${word} ${results.word[word]}`)
+      let word = Object.keys(results.word);
+      let count = results.word[word];
+      let upWord = word[0].toUpperCase();
+      $('#word').text(`${upWord}`);
+      $('#count').text(`${count}`);
     });
   }
 
@@ -13,6 +16,7 @@ $(document).ready(() => {
 
   $('#breakdown').on('click', function () {
     let value = $("textarea").val()
+    alert(value + " was submitted!")
     var words = value.split(" ")
     for (let counter = 0; counter < words.length; counter++) {
       $.post("https://wordwatch-api.herokuapp.com/api/v1/words", { word: { value: words[counter] } })
