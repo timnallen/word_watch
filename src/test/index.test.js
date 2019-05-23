610ef854faffd4bc286e56f1232e5bfeea95cc1a
Tim Allen <tnelsallen3@gmail.com>
2019-05-23 09:36:46 -0600

    Set up test skeleton

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -2,9 +2,9 @@ import { expect } from "chai"
 import sinon from "sinon"
 
 describe("Word watch functions", () => {
-  describe("", () => {
-    it("", () => {
+  describe("when I go to /", () => {
+    it("shows me the top word and count on the right side of the page", () => {
+      
     })
   })
 })
-
