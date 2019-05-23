3fe9be3380eb24036bea79280037b00ed0e8d4e3
Tim Allen <tnelsallen3@gmail.com>
2019-05-23 10:27:49 -0600

    Add function to get top word value upon login and everytime a new word is posted

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
-})
+  var getValue = function() {fetch("https://wordwatch-api.herokuapp.com/api/v1/top_word")
+    .then(response => response.json())
+    .then(results => {
+      let word = Object.keys(results.word)
+      $('h3').text(`Top results from Word Watch API: ${word} ${results.word[word]}`)
+    });
+  }
+
+  getValue();
+
+  $('#breakdown').on('click', function () {
+    let value = $("textarea").val()
+    var words = value.split(" ")
+    for (let counter = 0; counter < words.length; counter++) {
+      $.post("https://wordwatch-api.herokuapp.com/api/v1/words", { word: { value: words[counter] } })
+        .done(response => {
+          console.log(response);
+        });
+    }
+    getValue();
+  });
+});
